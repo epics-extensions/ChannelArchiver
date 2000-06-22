63d470d76a389090968a807f3f4db678157f5ba7
kasemir <kasemir>
2000-06-22 22:07:14 +0000

    Semaphore error messages

--- Engine/WriteThread.cpp
+++ Engine/WriteThread.cpp
@@ -11,37 +11,46 @@ USING_NAMESPACE_CHANARCH
 int WriteThread::run ()
 {
 #ifdef HAVE_SIGACTION
-	// Block signals
-	//
-	// There is a handler installed in main(),
-	// but we don't want to be interrupted
-	// in I/O calls for this thread:
-	sigset_t sigs_to_block;
-	sigemptyset (&sigs_to_block);
-	sigaddset (&sigs_to_block, SIGINT);
-	sigaddset (&sigs_to_block, SIGTERM);
-	if (pthread_sigmask (SIG_BLOCK, &sigs_to_block, 0))
-		LOG_MSG ("WriteThread cannot block signals\n");
+    // Block signals
+    //
+    // There is a handler installed in main(),
+    // but we don't want to be interrupted
+    // in I/O calls for this thread:
+    sigset_t sigs_to_block;
+    sigemptyset (&sigs_to_block);
+    sigaddset (&sigs_to_block, SIGINT);
+    sigaddset (&sigs_to_block, SIGTERM);
+    if (pthread_sigmask (SIG_BLOCK, &sigs_to_block, 0))
+        LOG_MSG ("WriteThread cannot block signals\n");
 #endif
 
-	LOG_MSG ("WriteThread started\n");
-	while (_go)
-	{
-		_wait.take ();
-		if (_go)
-		{
-			_writing = true;
-			try
-			{
-				theEngine->writeArchive (_now);
-			}
-			catch (ArchiveException &e)
-			{
-				LOG_MSG (osiTime::getCurrent() << ": WriteThread Cannot write, got\n\t" << e.what() << "\n");
-				_go = false;
-			}
-			_writing = false;
-		}
-	}
-	return 0;
+    LOG_MSG ("WriteThread started\n");
+    while (_go)
+    {
+        if (! _wait.take ())
+        {
+            LOG_MSG (osiTime::getCurrent()
+                     << ": WriteThread cannot take _wait semaphore,"
+                     << "quitting\n");
+            _go = false;
+        }
+        
+        if (_go)
+        {
+            _writing = true;
+            try
+            {
+                theEngine->writeArchive (_now);
+            }
+            catch (ArchiveException &e)
+            {
+                LOG_MSG (osiTime::getCurrent()
+                         << ": WriteThread Cannot write, got\n\t"
+                         << e.what() << "\n");
+                _go = false;
+            }
+            _writing = false;
+        }
+    }
+    return 0;
 }

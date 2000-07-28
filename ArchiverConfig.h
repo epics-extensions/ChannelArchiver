fefa63c3567d7ba31887f958f715ab5ee0bdb678
kasemir <kasemir>
2000-07-28 23:23:51 +0000

    *** empty log message ***

--- ArchiverConfig.h
+++ ArchiverConfig.h
@@ -19,9 +19,9 @@
 
 // This should match Make.ver
 #define VERSION 1
-#define RELEASE 4
+#define RELEASE 5
 
-#define VERSION_TXT "1.4"
+#define VERSION_TXT "1.5"
 
 // ----------------------------------------------------------
 // ArchiveEngine

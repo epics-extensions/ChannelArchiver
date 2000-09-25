3fabf25126e8743cd5c81087166a67240fee71ae
kasemir <kasemir>
2000-09-25 17:35:27 +0000

    Noborus version

--- Engine/Tools/stopEngine.py
+++ Engine/Tools/stopEngine.py
@@ -1,39 +1,56 @@
+#!/bin/env python
+# $Id$
 # Simple example how engine can be stopped via python.
 # - And since python can be called from a script:
 #   Example how to influence the engine from scripts ;-)
 #
 # -kuk-
-
-# Configure this:
-#
-machine="localhost"
-port=4812
-# leave user empty if you do not use the user/passwd mechanism
-user="engine"
-user = ""
-password="password"
-
-
 ##############################
 import urllib, sys, regex
+# Configure this:
+#
+# leave user empty if you do not use the user/passwd mechanism
+#       user="engine",
 
-if user:
-    url="http://%s:%d/stop?USER=%s&PASS=%s" % (machine, port, user, password)
-else:
-    url="http://%s:%d/stop" % (machine, port)
+def stop(machine="localhost",port=4812, user = "",password=""):
+    if user:
+        url="http://%s:%d/stop?USER=%s&PASS=%s" \
+             % (machine, port, user, password)
+    else:
+        url="http://%s:%d/stop" % (machine, port)
+    try:
+        url=urllib.urlopen (url)
+    except IOError, message:
+        print "Cannot connect to %s, error:%s"%(machine, message)
+        sys.exit (-1)
+        
+    page=url.read()
+    if regex.search ("Engine Stopped", page) >= 0:
+        print "Engine stopped"
+    else:
+        print "Cannot stop Engine, this is the raw page I got:"
+        print page
 
-try:
-    url=urllib.urlopen (url)
-except IOError, message:
-    print "Cannot connect, error:", message
-    sys.exit (-1)
+if __name__ == "__main__":
+    from sys import argv
+    import getopt,re
 
-page=url.read()
-if regex.search ("Engine Stopped", page) >= 0:
-    print "Engine stopped"
-else:
-    print "Cannot stop Engine, this is the raw page I got:"
-    print page
-
-
+    if len(argv) <= 1:
+        stop()
+    else:
+        opts, pargs= getopt.getopt(argv[1:],
+                                   "h:",
+                                   ["machine=","user=","password=",
+                                    "port=","help"])
+        kw={}
+        for key,val in opts:
+            key=re.sub("-+","",key)
+            kw[key]=val
+        if kw.has_key("help"):
+            print "Usage: %s [-h hostname] [--machin hostname] [--userusername] [--password password]" % argv[0]
+            sys.exit()
+        if kw.has_key("h") and not kw.has_key("machine"):
+            kw["machine"]=kw["h"]
+            del kw["h"]
+        apply(stop,(),kw)
 

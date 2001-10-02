b2d25af24af12ac67d9b75beee8bd2614955f0b1
birke <birke>
2001-10-02 21:48:24 +0000

    documentation update

--- LibIO/BucketingValueIteratorI.h
+++ LibIO/BucketingValueIteratorI.h
@@ -16,19 +16,25 @@
 //////////////////////////////////////////////////////////////////////
 //CLASS BucketingValueIteratorI
 // A CLASS ValueIterator that performs bucketing data reduction.
-//
+// <p>
 // For any set of values within a certain deltaT (the bucket)
 // at most 4 values are returned (first, min, max and last).
-//
+// <p>
 // When <I>next()/prev()</I> is called
 // this specialization of a CLASS ValueIteratorI
 // will move to the next/previous CLASS Value
 // as defined by the bucketing algorithm.
-//
-// Data reduction is not possible between "Info" values
-// line "Archive Off" or "Disabled".
+// <p>
+// Data reduction is not possible between <em>Info</em> values
+// line <tt>Archive Off</tt> or <tt>Disabled</tt>.
 // When such an info value is hit,
 // the iterator will return the info value.
+// <p>
+// <b>Up to now, the <em>BucketingValueIteratorI</em> is not capable of going 
+// backwards! For now it will throw an exception if <em>prev()</em> is called.</b>
+// <p>
+// The <em>BucketingValueIteratorI</em> only works properly for scalar
+// numeric datatypes.
 class BucketingValueIteratorI : public ValueIteratorI
 {
 public:
@@ -44,6 +50,9 @@ public:
 	// virtuals from ValueIteratorI
 	bool isValid () const;
 	const ValueI * getValue () const;
+
+        //* Up to now, <I>prev()</I> will raise an
+        //  <tt>Illegal</tt>-Exception.
 	bool next ();
 	bool prev ();
 
@@ -59,14 +68,21 @@ private:
 
 	ValueIteratorI *_base;
 	osiTime _time;
+        //* In the <em>current</em> bucket, these four values, the
+        //  ControlInfo and the appropriate status and severity are the
+        //  ones we're interested in.
         double _first;
    	double _min;
    	double _max;
    	double _last;
         dbr_short_t _status, _sevr;
         const CtrlInfoI *_ctrlinfo;
+
+        //* The <em>bucketing</em> algorithm is based on 2 nested state
+        //  machines.
         enum { S_INIT, S_VALUES, S_OUT } _state;
         enum { V_FIRST, V_MIN, V_MAX, V_LAST, V_NONE } _oval;
+
 	double _deltaT;
 	ValueI *_value;
         enum { D_BCK, D_FWD };

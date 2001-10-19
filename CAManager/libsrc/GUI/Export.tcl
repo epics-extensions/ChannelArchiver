edaa6ad39bdbcb25cd8f335f23457248f5305097
birke <birke>
2001-10-19 19:32:21 +0000

    reorganized arguments for different export options automatically fill in selected archive

--- CAManager/libsrc/GUI/Export.tcl
+++ CAManager/libsrc/GUI/Export.tcl
@@ -1,7 +1,10 @@
 proc camGUI::aExport {w} {
   set initialdir [pwd]
+  set arch ""
   if [regexp (\[0-9\]*), [$w cursel] all row] {
     set initialdir [file dirname [camMisc::arcGet $row cfg]]
+    set arch [clock format [clock seconds] -format $initialdir/[camMisc::arcGet $row archive]]
+    if {![file exists $arch]} {set arch ""}
   }
   set f [actionDialog "Export Archive"]
   selFile $f.s "Select source Archive" ::var(xport,$::tl_cnt,src) $initialdir archive
@@ -18,7 +21,11 @@ proc camGUI::aExport {w} {
       -variable ::var(xport,$::tl_cnt,ef) -value "Matlab" -anchor w
   radiobutton $cs.g -text "a Gnuplot file" \
       -variable ::var(xport,$::tl_cnt,ef) -value "Gnuplot" -anchor w
-  $cs.a invoke
+  if {"$arch" != ""} {
+    set ::var(info,$::tl_cnt,src) $arch
+  } else {
+    $cs.a invoke
+  }
   pack $cs.a $cs.s $cs.m $cs.g -side top -fill x
   pack $f.ef -side left -fill x -expand t
 
@@ -62,6 +69,12 @@ proc camGUI::aExport {w} {
   set nb [$f.onb add -label Gnuplot]
   iwidgets::labeledframe $nb.o -labeltext "Options" -labelpos nw -ipadx 4 -ipady 4
   set cs [$nb.o childsite]
+  frame $cs.o2
+  iwidgets::entryfield $cs.o2.reduce -labeltext "reduce data to" \
+      -textvariable ::var(xport,$::tl_cnt,opt,reduce) -width 6 -validate numeric
+  label $cs.o2.l -text "buckets"
+  pack $cs.o2.reduce $cs.o2.l -side left -fill x
+  pack $cs.o2 -side top -fill x
   pack $nb.o -side left -expand t -fill both
 
   set nb [$f.onb add -label Matlab]
@@ -78,46 +91,30 @@ proc camGUI::aExport {w} {
   pack $f -side top -fill x
   set f [file rootname $f]
   selFile $f.d "Select target" ::var(xport,$::tl_cnt,target) $initialdir archive Save
-  iwidgets::combobox $f.rx -labeltext "Regular Expression to match Channelnames" -labelpos nw \
-      -textvariable ::var(xport,$::tl_cnt,regex)
+  combobox $f.rx "Regular Expression to match Channelnames" top \
+      ::var(xport,$::tl_cnt,regex) [luniq $::selected(regex)]
   pack $f.rx -side top -fill x -padx 4 -pady 4
   frame $f.t -bd 0
   frame $f.t.f -bd 0
   frame $f.t.t -bd 0
-#  checkbutton $f.t.f.b -text "From the very beginning" -variable ::var(xport,$::tl_cnt,tf) \
-      -command "$f.t.f.nb view \[expr 1 - \$::var(xport,$::tl_cnt,tf)]"
-#  checkbutton $f.t.t.b -text "To the very end" -variable ::var(xport,$::tl_cnt,tt) \
-      -command "$f.t.t.nb view \[expr 1 - \$::var(xport,$::tl_cnt,tt)]"
-#  iwidgets::notebook $f.t.f.nb  -height .33i -width 3i -background [$f.t.f.b cget -background]
-#  iwidgets::notebook $f.t.t.nb  -height .33i -width 3i -background [$f.t.t.b cget -background]
-#  set p [$f.t.f.nb add -label 0]
-#  set p [$f.t.f.nb add -label 1]
+
   set p $f.t.f
-#  $f.t.f.nb view 1
   label $p.l -text From: -width 10 -anchor e
   iwidgets::dateentry $p.d -iq high
   iwidgets::timeentry $p.t -format military
   pack $p.l $p.d $p.t -side left
   set ::tf $p
 
-#  set p [$f.t.t.nb add -label 0]
-#  set p [$f.t.t.nb add -label 1]
   set p $f.t.t
-#  $f.t.t.nb view 1
   label $p.l -text To: -width 10 -anchor e
   iwidgets::dateentry $p.d -iq high
   iwidgets::timeentry $p.t -format military
   pack $p.l $p.d $p.t -side left
   set ::tt $p
 
-#  pack $f.t.f.b $f.t.f.nb -side top -fill x
-#  pack $f.t.t.b $f.t.t.nb -side top -fill x
   grid $f.t.f $f.t.t
   pack $f.t -fill both -expand t -side top
 
-  eval $f.rx insert list end [luniq $::selected(regex)] \"\"
-#  $f.t.f.b invoke
-#  $f.t.t.b invoke
   $f.s.b invoke
   $f.d.b invoke
   while {[set res [handleDialog $f \
@@ -135,11 +132,19 @@ proc camGUI::aExport {w} {
       }
     } else {
       set cmd ArchiveExport
-      if {$::var(xport,$::tl_cnt,opt,fill)} { lappend cmd -fill }
-      if {$::var(xport,$::tl_cnt,opt,text)} { lappend cmd -text }
-      if {($::var(xport,$::tl_cnt,opt,interpol) != "") && 
-	  ($::var(xport,$::tl_cnt,opt,interpol) > 0)} {
-	lappend cmd -interpolate $::var(xport,$::tl_cnt,opt,interpol)
+      if {("$::var(xport,$::tl_cnt,ef)" == "Spreadsheet")} {
+	if {($::var(xport,$::tl_cnt,opt,interpol) != "") && 
+	    ($::var(xport,$::tl_cnt,opt,interpol) > 0)} {
+	  lappend cmd -interpolate $::var(xport,$::tl_cnt,opt,interpol)
+	}
+	if {$::var(xport,$::tl_cnt,opt,fill)} { lappend cmd -fill }
+      }
+      if {("$::var(xport,$::tl_cnt,ef)" == "Gnuplot") && 
+	  ($::var(xport,$::tl_cnt,opt,reduce) != "") && 
+	  ($::var(xport,$::tl_cnt,opt,reduce) > 0)} {
+	lappend cmd -reduce $::var(xport,$::tl_cnt,opt,reduce)
+      } else {
+	if {$::var(xport,$::tl_cnt,opt,text)} { lappend cmd -text }
       }
     }
 

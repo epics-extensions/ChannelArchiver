bc82f7ae62a1d6400f456aad5cd0c5915d1b1b91
birke <birke>
2001-10-19 19:32:29 +0000

    automatically fill in selected archive

--- CAManager/libsrc/GUI/Info.tcl
+++ CAManager/libsrc/GUI/Info.tcl
@@ -1,11 +1,18 @@
 proc camGUI::aInfo {w} {
   set initialdir [pwd]
+  set arch ""
   if [regexp (\[0-9\]*), [$w cursel] all row] {
     set initialdir [file dirname [camMisc::arcGet $row cfg]]
+    set arch [clock format [clock seconds] -format $initialdir/[camMisc::arcGet $row archive]]
+    if {![file exists $arch]} {set arch ""}
   }
   set f [actionDialog "Info Archive"]
   selFile $f.c "Select Archive to get Info about" ::var(info,$::tl_cnt,arc) $initialdir archive
-  $f.c.b invoke
+  if {"$arch" != ""} {
+    set ::var(info,$::tl_cnt,arc) $arch
+  } else {
+    $f.c.b invoke
+  }
   while {[handleDialog $f]} {
     pXec {ArchiveManager -info $::var(info,$::tl_cnt,arc)} $f.tf.t
   }

bbd9d14042dbdfa29fdf325ff0d3e8e927c01847
birke <birke>
2001-10-23 16:07:23 +0000

    altered comments to make them appear in autogenerated doc added function to create new value in archive

--- casi/archive.h
+++ casi/archive.h
@@ -1,55 +1,61 @@
-
+class value;
 class channel;
 
 class archive
 {
 public:
-	/* Create new archive object.
-	   Use open to connect it to existing archive file,
-       write to create new one or open for append
-	 */
-	archive ();
+   archive ();
+   /* Create new archive object.
+      Use open to connect it to existing archive file,
+      write to create new one or open for append
+   */
 
-	/* Remove archive object */
-	~archive ();
+   ~archive ();
+   /* Remove archive object */
 
-	/* Open archive data source */
-	bool open (const char *name);
+   bool open (const char *name);
+   /* Open archive data source */
 
-	/* Similar: Position channel object on first channel in archive */
-	bool findFirstChannel (channel &channel);
+   bool findFirstChannel (channel &channel);
+   /* Similar: Position channel object on first channel in archive */
 
-	/* Try to find channel by given name,
-	   make channel class point to that channel.
+   bool findChannelByName (const char *name, channel &channel);
+   /* Try to find channel by given name,
+      make channel class point to that channel.
 
-	   Result: succesful?
-	 */
-	bool findChannelByName (const char *name, channel &channel);
+      Result: succesful?
+   */
 
-	/* Find channel by pattern (regular expression) */
-	bool findChannelByPattern (const char *pattern, channel &channel);
+   bool findChannelByPattern (const char *pattern, channel &channel);
+   /* Find channel by pattern (regular expression) */
 
-    const char *name ();
+   const char *name ();
+   /* return the name of the (already opened) archive file */
     
-    /* Support for write access:
-	 * Create new archive or open existing one for appending
-     */
-	bool write (const char *name, double hours_per_file);
+   bool write (const char *name, double hours_per_file);
+   /* Support for write access:
+    * Create new archive or open existing one for appending
+    */
 
-    /* Add a new channel to this archive.
-     * It's an error to add a channel name that already exists.
-     * This is also only supported after succesfully calling 'write'
-     */
-    bool addChannel (const char *name, channel &channel);
+   bool addChannel (const char *name, channel &channel);
+   /* Add a new channel to this archive.
+    * It's an error to add a channel name that already exists.
+    * This is also only supported after succesfully calling 'write'
+    */
     
-    /* Helper if you want to determine the suggested time
-     * for a new file, usually used in conjuntion with value.determineChunk()
-     */
-    const char *nextFileTime (const char *current_time);
-    
+   const char *nextFileTime (const char *current_time);
+   /* Helper if you want to determine the suggested time
+    * for a new file, usually used in conjuntion with value.determineChunk()
+    */
+
+
+   void newValue (int type, int count, value &val);
+   /* Create a new value with the given type and count, suitable for this
+      type of archive (will fail on e.g. MultiArchive!) */
+
 private:
-	stdString _name;
-	ArchiveI *_archiveI;
+   stdString _name;
+   ArchiveI *_archiveI;
 };
 
 

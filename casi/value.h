9cf3118aa4c627876f14ef3ed1bacc19e35f1937
birke <birke>
2001-10-23 16:07:34 +0000

    added functions to get/set value, controlinfo, timestamp, status/severity of a 'value' added function to 'clone()' a value

--- casi/value.h
+++ casi/value.h
@@ -1,66 +1,112 @@
+class ctrlinfo;
 
 class value
 {
 public:
-	value();
-	~value();
+   value();
+   ~value();
 
-	/* Is this value valid?
-	   The following calls are only allowed if the value is valid!
-	 */
-	bool valid(); 
+   void clone(value&);
+   /* "Clone" an existing value by copying all value-, time-, status- and
+      control-info
+   */
 
-	/* Some values are not numeric values but purely informational. 
-		In that case the status method returns 
+   bool valid() const; 
+   /* Is this value valid?
+      The following calls are only allowed if the value is valid!
+   */
+   
+   bool isInfo() const;
+   /* Some values are not numeric values but purely informational. 
+      In that case the status method returns 
+      
+      Archive_Off,
+      Disconnected,
+      Disabled,
+      ... maybe more special values
 
-		Archive_Off,
-		Disconnected,
-		Disabled,
-		... maybe more special values
+      To check for such "info" values, this command can be used. 
+   */
 
-	   To check for such "info" values, this command can be used. 
-	 */
-	bool isInfo();
+   const char *type() const;
+   /* Get native type of this value as string */
 
-	/* Get native type of this value as string */
-	const char *type();
+   int count() const;
+   /* Get array size of this value (0: invalid, 1: scalar) */
 
-	/* Get array size of this value (0: invalid, 1: scalar) */
-	int count();
+   double get() const { return getidx(0); };
+   /* Get current value as number, same as getidx(0) */
 
-	/* Get current value as number, same as getidx(0) */
-	double get();
+   double getidx(int index) const; 
+   /* Get current value as number,
+    * accessing the specified array element
+    */
 
-	/* Get current value as number,
-	   accessing the specified array element
-	 */
-	double getidx(int index); 
+   void set(double v) { setidx(v, 0); };
+   /* set the current value as double (requires write-accessible value!) */
 
-	/* Get current value as text */
-	const char *text();
+   void setidx(double v, int index);
+   /* set the value with index index as double (requires write-accessible
+    * value!) */
 
-	/* Get current time stamp */
-	const char *time();
+   void parse( const char* );
+   /* extract a new value from the string passed */
 
-    /* Return current time stamp as double */
-    double getDoubleTime();
+   const char *text() const;
+   /* Get current value as text */
 
-	/* Get current status string */
-	const char *status();
+   const char *time() const;
+   /* Get current time stamp */
 
-	/* Position on next value. Result: succesful? */
-	bool next();
+   bool isRepeat() const;
+   /* true if the value is a repeat-value */
 
-	/* Position on prev value. Result: succesful? */
-	bool prev();
+   void setTime(const char *);
+   /* set the timestamp of the current value (requires write-accessible
+    * value!) */
 
-    /* Count values of matching type/control information
-     * up to given time stamp */
-    int determineChunk(const char *until);
+   double getDoubleTime() const;
+   /* Return current time stamp as double
+    */
+
+   const char *status() const;
+   /* Get status of current value as string
+    */
+
+   int stat() const;
+   /* Get status of current value as integer */
+
+   int sevr() const;
+   /* Get severity of current value as integer */
+
+   void setStat(int stat, int sevr);
+   void setStatus( const char* );
+   /* Set status and severity of the current value
+    * (requires write-accessible value!) */
+
+   bool next();
+   /* Position on next value. Result: succesful? */
+
+   bool prev();
+   /* Position on prev value. Result: succesful? */
+
+   int determineChunk(const char *until);
+   /* Count values of matching type/control information
+    * up to given time stamp */
+
+   void getCtrlInfo(ctrlinfo&) const;
+   void setCtrlInfo(ctrlinfo&);
+   /* get/set the control info of the current value
+    * (setting requires write-accessible value!) */
 
 private:
-	friend class channel;
+   friend class channel;
+   friend class archive;
 
-	void setIter(ValueIteratorI *iter);
-	ValueIteratorI *_iter;
+   const ValueI *getVal() const;
+
+   void setIter(ValueIteratorI *iter);
+   ValueIteratorI *_iter;
+   ValueI *_valI;
 };
+

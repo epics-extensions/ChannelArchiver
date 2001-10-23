a1b086bb7658dc21d2c40e9c0ffffb51e18633be
birke <birke>
2001-10-23 16:07:31 +0000

    altered comments to make them appear in autogenerated doc

--- casi/channel.h
+++ casi/channel.h
@@ -3,62 +3,67 @@ class value;
 class channel
 {
 public:
-	channel ();
-	~channel ();
+   channel ();
+   ~channel ();
 
-	/* Is this channel valid?
-	   The following operations are only allowed
-	   when the channel is valid!
-	 */
-	bool valid (); 
+   bool valid () const; 
+   /* Is this channel valid?
+      The following operations are only allowed
+      when the channel is valid!
+   */
 
-	/* Get name of current channel */
-	const char *name (); 
+   const char *name () const; 
+   /* Get name of current channel */
 
-	/* Position on next channel. Result: succesful? */
-	bool next (); 
+   bool next (); 
+   /* Position on next channel. Result: succesful? */
 
-	/* Get first available time stamp of current channel */
-	const char *getFirstTime (); 
+   const char *getFirstTime () const; 
+   /* Get first available time stamp of current channel */
 
-	/* Get last available time stamp of current channel */
-	const char *getLastTime (); 
+   const char *getLastTime () const; 
+   /* Get last available time stamp of current channel */
 
-	/* Get first available value of current channel */
-	bool getFirstValue (value &value); 
+   bool getFirstValue (value &value) const; 
+   /* Get first available value of current channel */
 
-	/* Get last available value of current channel */
-	bool getLastValue (value &value); 
+   bool getLastValue (value &value) const; 
+   /* Get last available value of current channel */
 
-	/* Get first value in archive that follows given time stamp */
-	bool getValueAfterTime (const char *time, value &value); 
+   bool getValueAfterTime (const char *time, value &value) const; 
+   /* Get first value in archive that follows given time stamp */
 
-	/* Get first value in archive that preceeds given time stamp */
-	bool getValueBeforeTime (const char *time, value &value); 
+   bool getValueBeforeTime (const char *time, value &value) const; 
+   /* Get first value in archive that preceeds given time stamp */
 
-	/* Get value in archive that is closest to given time stamp */
-	bool getValueNearTime (const char *time, value &value); 
+   bool getValueNearTime (const char *time, value &value) const; 
+   /* Get value in archive that is closest to given time stamp */
 
-    /* Write support:
-     * Lock current buffer, return number of free entries
-     * for values of given type
-     */
-     int lockBuffer (const value &value);
+   int lockBuffer (const value &value);
+   /* Write support:
+    * Lock current buffer, return number of free entries
+    * for values of given type
+    */
 
-     /* Call this if current buffer is full */
-     void addBuffer (const value &value, int value_count);
+   void addBuffer (const value &value, int value_count);
+   /* Call this if current buffer is full */
 
-     /* Add new value, call lock/addBuffer before */
-     bool addValue (const value &value);
+   bool addValue (const value &value);
+   /* Add new value, calls lock/addBuffer if needed */
 
-     /* Call after valued were added to update pointers */
-     void releaseBuffer ();
+   void releaseBuffer ();
+   /* Call after valued were added to update pointers */
     
 private:
-	friend class archive;
+   friend class archive;
 
-	void setIter (ArchiveI *archiveI);
-	bool testValue (value &value); 
-	ArchiveI *_archiveI;
-	ChannelIteratorI *_iter;
+   void setIter (ArchiveI *archiveI);
+   bool testValue (value &value) const; 
+   ArchiveI *_archiveI;
+   ChannelIteratorI *_iter;
+
+   size_t buf_size;
+   static const size_t init_buf_size;
+   static const size_t max_buf_size;
+   static const size_t buf_size_fact;
 };

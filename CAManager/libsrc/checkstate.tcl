2900ecfd9702c05990f60bfd0fa39520ee528851
birke <birke>
2001-10-30 21:25:58 +0000

    * eliminated some race conditions

--- CAManager/libsrc/checkstate.tcl
+++ CAManager/libsrc/checkstate.tcl
@@ -3,24 +3,19 @@ proc checkstate {arr ind op} {
     trace vdelete ::_run($ind) w checkstate
     if {("[camMisc::arcGet $ind start]" == "NO")} {return}
     if {("[camMisc::arcGet $ind start]" != "timerange")} {
-      set ::sched($ind,start,job) [after 1 "runArchiver $ind"]
+      catch {after cancel $::sched($ind,start,job)}
+      set ::sched($ind,start,job) [after 100 "runArchiver $ind"]
     } else {
-      set timespec [camMisc::arcGet $ind timespec]
-      set Starttime [clock scan "[lindex $timespec 0] [lindex $timespec 1]"]
+      # timerange
+      lassign [duetime $ind] Starttime Stoptime
       set starttime [expr ( $Starttime - [clock seconds] ) * 1000]
-      set stoptime [clock scan "[lindex $timespec 3] [lindex $timespec 4]"]
-      set stoptime [expr ( $stoptime - [clock seconds] ) * 1000]
+      set stoptime [expr ( $Stoptime - [clock seconds] ) * 1000]
       if {$stoptime <= 0} return
       if {$starttime < 0} {set starttime 0}
       if {[info exists ::sched($ind,start,time)] && ($::sched($ind,start,time) == $Starttime)} return
-      if {$starttime != 0} {
-#	Puts "starting \"[camMisc::arcGet $ind descr]\" in [expr $starttime / 1000]s" schedule
-	Puts "starting \"[camMisc::arcGet $ind descr]\" @ [httpd::time $Starttime]" schedule
-      }
-      if {$starttime >= 0} {
-	set ::sched($ind,start,time) $Starttime
-	set ::sched($ind,start,job) [after $starttime "runArchiver $ind"]
-      }
+      Puts "starting \"[camMisc::arcGet $ind descr]\" @ [httpd::time $Starttime]" schedule
+      set ::sched($ind,start,time) $Starttime
+      set ::sched($ind,start,job) [after $starttime "runArchiver $ind"]
     }
   } elseif [regexp "^(BLOCKED|since)" $::_run($ind)] {
     trace vdelete ::_run($ind) w checkstate
--- CAManager/libsrc/scheduleStop.tcl
+++ CAManager/libsrc/scheduleStop.tcl
@@ -1,15 +1,13 @@
 proc scheduleStop {i} {
   set now [clock seconds]
-  set duetime [lindex [duetime $i] 1]
-  if {$duetime < 0} return
+  lassign [duetime $i] starttime stoptime
+  if {$starttime > $now} return
+  if {$stoptime < $now} return
 
-  camMisc::arcSet $i duetime $duetime
-  if {$duetime > 0} {
-    if {[info exists ::sched($i,stop,time)] && ($::sched($i,stop,time) == $duetime)} return
-    catch {after cancel $::sched($i,stop,job)}
-#    Puts "stopping \"[camMisc::arcGet $i descr]\" @ [expr ( $duetime - $now )]s" schedule
-    Puts "stopping \"[camMisc::arcGet $i descr]\" @ [httpd::time $duetime]" schedule
-    set ::sched($i,stop,job) [after [expr ( $duetime - $now ) * 1000] "stopArchiver $i"]
-    set ::sched($i,stop,time) $duetime
-  }
+  camMisc::arcSet $i stoptime $stoptime
+  if {[info exists ::sched($i,stop,time)] && ($::sched($i,stop,time) == $stoptime)} return
+  catch {after cancel $::sched($i,stop,job)}
+  Puts "stopping \"[camMisc::arcGet $i descr]\" @ [httpd::time $stoptime]" schedule
+  set ::sched($i,stop,job) [after [expr ( $stoptime - $now ) * 1000] "stopArchiver $i"]
+  set ::sched($i,stop,time) $stoptime
 }

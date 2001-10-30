4006fc7b2d34865219493426efdb990c7c1d8d65
birke <birke>
2001-10-30 21:25:50 +0000

    * interval to check for running archivers configurable * eliminated some race conditions

--- CAManager/libsrc/checkArchivers.tcl
+++ CAManager/libsrc/checkArchivers.tcl
@@ -1,11 +1,10 @@
 proc checkArchivers {args} {
   trace vdelete ArchiversSet w {after 1 checkArchivers}
-  after 3000 checkArchivers
+  after [expr 1000 * $::bgCheckInt] checkArchivers
   foreach i [camMisc::arcIdx] {
-    set ::_run($i) NO
+    if {![info exists ::_run($i)]} {set ::_run($i) NO}
     if {"[camMisc::arcGet $i host]" != "$::_host"} continue
-#    if {"[camMisc::arcGet $i start]" == "NO"} continue
-    if {![info exist ::_run($i)] || ![regexp "^since" $::_run($i)]} {set ::_run($i) -}
+#    if {![regexp "^since" $::_run($i)]} {set ::_run($i) -}
     trace variable ::_run($i) w checkstate
     camComm::CheckRunning $i ::_run($i)
   }

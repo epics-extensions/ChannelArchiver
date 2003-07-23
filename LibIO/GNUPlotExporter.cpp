89f3a435a0ac8428038d60af800026b47b8fc623
kasemir <kasemir>
2003-07-23 14:14:26 +0000

    guard against RH 9 problems with isfinite

--- LibIO/GNUPlotExporter.cpp
+++ LibIO/GNUPlotExporter.cpp
@@ -14,6 +14,8 @@
 #include "epicsTimeHelper.h"
 #include <epicsMath.h> // math.h + isinf + isnan
 
+// Under RH9, isfinite is defined as a macro
+#undef isfinite
 static int isfinite(double value)
 {
     return !(isinf(value) ||  isnan(value));

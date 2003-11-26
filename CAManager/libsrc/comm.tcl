ee18be8355af99a71cd6057e2120fa8dc2b2caac
birke <birke>
2003-11-26 11:46:29 +0000

    use ::http::geturl instead of handling the socket myself

--- CAManager/libsrc/comm.tcl
+++ CAManager/libsrc/comm.tcl
@@ -13,45 +13,51 @@ regsub ": (.*) \\$" $CVS(Author,Comm) "\\1" CVS(Author,Comm)
 namespace eval camComm {
 }
 
-proc camComm::CheckRunning {i rvar} {
-  global fstate fsvar fsto
+package require http
 
+proc camComm::CheckRunning {i rvar} {
   regexp "\\((\[^,\]*)\[,\\)\]" $rvar all row
   if {![info exists ::lastArc($row)]} {set ::lastArc($row) ""}
   set start [clock seconds]
-  if [catch {set sock [socket [camMisc::arcGet $i host] [camMisc::arcGet $i port]]}] {
-    if {[file exists [Blockfile $i]]} {
-      condSet $rvar "BLOCKED"
-    } else {
-      if {[info exists ::tk_version]} {
-	condSet $rvar "NO"
-      } else {
-	Puts "setting $rvar to \"NO\"" debug3
-	catch {set $rvar "NO"}
+#  set $rvar "???"
+  if {[catch {::http::geturl "http://[camMisc::arcGet $i host]:[camMisc::arcGet $i port]/"} token]} {
+    condSet $rvar "NO"
+  } else {
+    set ::fsvar($token) $rvar
+    set ::fsto($token) [after 5000 "camComm::processTimeout $token; update"]
+    upvar #0 $token state
+    lappend ::socks $state(sock)
+    array set meta $state(meta)
+
+    while {$state(status) != "ok"} {
+      update
+      after 100
+    }
+
+    if [info exists state(error)] {
+      condSet $::fsvar($token) "NO"
+    }
+
+    if {![regexp "ArchiveEngine" $meta(Server)]} {
+      condSet ::lastArc($row) "unknown Server"
+      camComm::Close $token
+    }
+    foreach line [split $state(body) "\n"] {
+      if {[regexp ".*Started.*>(\[^<\]+)<" $line all started]} {
+	condSet $::fsvar($token) "since [string range $started 0 18]"
+      } elseif {[regexp ".*Archive.*>(\[^<\]+)<" $line all archive]} {
+	condSet ::lastArc($row) "$archive"
       }
     }
-    return
+    camComm::Close $token
   }
-  lappend ::Socks $sock
-
-  puts $sock "GET / HTTP/1.0"
-  puts $sock ""
-  flush $sock
-
-  set fsvar($sock) $rvar
-  set fsvar($sock,arc) ::lastArc($row)
-  set fstate($sock) open
-  set ::bytes($sock) ""
-  fileevent $sock readable "camComm::processAnswer $sock; update"
-  set fsto($sock) [after 5000 "camComm::processTimeout $sock; update"]
 }
 
-proc camComm::Close {sock} {
-  global fstate fsto
-  catch {after cancel $fsto($sock)}
-  set fstate($sock) closed
-  close $sock
-  if {[set i [lsearch -exact $::socks $sock]] >= 0} { set ::socks [lreplace $::socks $i $i] }
+proc camComm::Close {token} {
+  upvar #0 $token state
+  catch {after cancel $::fsto($token)}
+  if {[set i [lsearch -exact $::socks $state(sock)]] >= 0} { set ::socks [lreplace $::socks $i $i] }
+  array unset $token
 }
 
 proc camComm::condSet {var val} {
@@ -60,76 +66,8 @@ proc camComm::condSet {var val} {
   }
 }
 
-set ::inAnswer 0
-proc camComm::processAnswer {sock} {
-  if {$::inAnswer} return
-  set ::inAnswer 1
-  global fstate fsvar
-  append ::bytes($sock) [read $sock]
-  set bl [string bytelength $::bytes($sock)]
-  if {[eof $sock]} { append ::bytes($sock) "\n" }
-
-  set nl [string first "\n" $::bytes($sock)]
-  # no full line yet...
-  while {$nl >= 0} {
-    set nl [string first "\n" $::bytes($sock)]
-    set line [string range $::bytes($sock) 0 [expr $nl - 1]]
-    set ::bytes($sock) [string replace $::bytes($sock) 0 $nl ""]
-
-    switch $fstate($sock) {
-      open {
-	if {![regexp "^HTTP/.* 200 OK" $line]} {
-	  condSet $fsvar($sock) "invalid response"
-	  camComm::Close $sock
-	} else {
-	  set fstate($sock) http
-	}
-      }
-      http {
-	if {[regexp "^Server: (.*)" $line all server]} {
-	  if {"$server" != "ArchiveEngine"} {
-	    condSet $fsvar($sock) "unknown Server"
-	    camComm::Close $sock
-	  } else {
-	    set fstate($sock) server
-	  }
-	}
-      }
-      server {
-	if {"$line" == ""} {
-	  set fstate($sock) body
-	}
-      }
-      body {
-	if {[regexp ".*Started.*>(\[^<\]+)<" $line all started]} {
-	  condSet $fsvar($sock) "since [string range $started 0 18]"
-	  set fstate($sock) started
-	}
-      }
-      started {
-	if {[regexp ".*Archive.*>(\[^<\]+)<" $line all archive]} {
-	  condSet $fsvar($sock,arc) "$archive"
-	  set fstate($sock) end
-	}
-      }
-      end {
-	if {[eof $sock]} {
-	  camComm::Close $sock
-	}
-      }
-      closed {
-      }
-    }
-    set ::inAnswer 0
-  }
-}
-
-proc camComm::processTimeout {sock} {
-  global fstate fsvar fsto
-  if {"$fstate($sock)" != "closed"} {
-    condSet $fsvar($sock) "TIMEOUT"
-    set fstate($sock) closed
-    camComm::Close $sock
-  }
+proc camComm::processTimeout {token} {
+  condSet $::fsvar($token) "TIMEOUT"
+  camComm::Close $token
   update
 }

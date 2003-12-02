49a05c9d6f1b49fac326c79e0e73a2398b6ad68e
kasemir <kasemir>
2003-12-02 17:22:17 +0000

    DummyServer.cpp seems to implement the protocol

--- XMLRPCServer/DummyServer.cpp
+++ XMLRPCServer/DummyServer.cpp
@@ -88,57 +88,73 @@ xmlrpc_value *get_names(xmlrpc_env *env,
     return result;
 }
 
-static xmlrpc_value *make_ctrl_info(xmlrpc_env *env)
+static xmlrpc_value *make_ctrl_info(xmlrpc_env *env,
+                                    const char *units)
 {
 	xmlrpc_value *meta =
         xmlrpc_build_value(env,
                            STR("{s:i,s:d,s:d,s:d,s:d,s:d,s:d,s:i,s:s}"),
-                           "type", 1,
+                           "type", (xmlrpc_int32)1,
                            "disp_high",  10.0, "disp_low",    0.0,
                            "alarm_high", 10.0, "warn_high",   9.0,
                            "warn_low",    1.0, "alarm_low",   0.0,
-                           "prec",          4, "units",     "a.u.");
+                           "prec", (xmlrpc_int32)4,
+                           "units",     units);
 	return meta;
-}
-
-static xmlrpc_value *make_value(xmlrpc_env *env)
-{
-	xmlrpc_value *value =
-        xmlrpc_build_value(env,
-                           STR("{s:i,s:i,s:i,s:i,s:d}"),
-                           "status", 0,
-                           "severity", 0,
-                           "secs", time(0),
-                           "nano", 42,
-                           "value", 3.14);
-	return value;
 }
 
 xmlrpc_value *get_values(xmlrpc_env *env,
                          xmlrpc_value *args,
                          void *user)
 {
-    int i;
-    xmlrpc_value *result, *channel, *meta, *values, *value;
+    xmlrpc_value *names;
+    xmlrpc_value *name_val, *result, *channel, *meta, *values, *value;
+    xmlrpc_int32 start_sec, start_nano, end_sec, end_nano, count, how;
+    xmlrpc_int32 secs, nano;
+    xmlrpc_int32 name_count, name_index, name_len, i;
+    char *name;
 
+    // Extract arguments
+    xmlrpc_parse_value(env, args, STR("(Aiiiiii)"),
+                       &names,
+                       &start_sec, &start_nano, &end_sec, &end_nano,
+                       &count, &how);
+    name_count = xmlrpc_array_size(env, names);
+    // Build result for each requested channel name
     result = xmlrpc_build_value(env, STR("()"));
-
-    meta = make_ctrl_info(env);
-    values = xmlrpc_build_value(env, STR("()"));
-    for (i=0; i<10; ++i)
+    for (name_index = 0; name_index < name_count; ++name_index)
     {
-        value = make_value(env);
-        xmlrpc_array_append_item(env, values, value);
-        xmlrpc_DECREF(value);
-    }    
-
-    channel = xmlrpc_build_value(env, STR("{s:V,s:V}"),
-                                 "meta", meta, "values", values);
-    xmlrpc_DECREF(meta);
-    xmlrpc_DECREF(values);
-
-    xmlrpc_array_append_item(env, result, channel);
-    xmlrpc_DECREF(channel);
+        // extract name from array (no new ref!)
+        name_val = xmlrpc_array_get_item(env, names, name_index);
+        xmlrpc_parse_value(env, name_val, STR("s#"),
+                           &name, &name_len);                       
+        // Meta information
+        meta = make_ctrl_info(env, name);
+        // Values
+        values = xmlrpc_build_value(env, STR("()"));
+        for (i=0; i<count; ++i)
+        {
+            secs = start_sec + i*(end_sec - start_sec)/count;
+            nano = start_nano + i*(end_nano - start_nano)/count;
+            value = xmlrpc_build_value(env,
+                                       STR("{s:i,s:i,s:i,s:i,s:d}"),
+                                       "stat", (xmlrpc_int32)0,
+                                       "sevr", (xmlrpc_int32)0,
+                                       "secs", (xmlrpc_int32)secs,
+                                       "nano", (xmlrpc_int32)nano,
+                                       "value", 3.14+i);
+            xmlrpc_array_append_item(env, values, value);
+            xmlrpc_DECREF(value);
+        }
+        // Assemble channel = { Meta, Values }
+        channel = xmlrpc_build_value(env, STR("{s:V,s:V}"),
+                                     "meta", meta, "values", values);
+        xmlrpc_DECREF(meta);
+        xmlrpc_DECREF(values);
+        // Add to result array
+        xmlrpc_array_append_item(env, result, channel);
+        xmlrpc_DECREF(channel);
+    }
 
     return result;
 }
@@ -157,7 +173,7 @@ int main(int argc, const char *argv[])
                                 STR("Get channel names"));
     xmlrpc_cgi_add_method_w_doc(STR("archiver.get_values"),
                                 &get_values, 0,
-                                STR("A:s"),
+                                STR("A:siiiiii"),
                                 STR("Get values"));
     xmlrpc_cgi_process_call();
     xmlrpc_cgi_cleanup();
--- XMLRPCServer/test.pl
+++ XMLRPCServer/test.pl
@@ -1,27 +1,96 @@
+# THE perl script for testing the Archiver's XML-RPC server.
+
 use Frontier::Client;
 
 # Setup URL
-$server_url = 'http://bogart.ta53.lanl.gov/cgi-bin/xmlrpc/ArchiveServer.cgi';
+#$server_url = 'http://localhost/archdat/ArchiveServer.cgi';
+$server_url = 'http://bogart.ta53.lanl.gov/cgi-bin/xmlrpc/DummyServer.cgi';
 $server = Frontier::Client->new(url => $server_url);
 
 # { int32 ver, string desc } = archdat.info()
-$result = $server->call('archdat.info');
+$result = $server->call('archiver.info');
 printf("Archive Data Server V %d,\nDescription '%s'\n\n",
        $result->{'ver'},
        $result->{'desc'});
 
 # string name[] = archdat.get_names(string pattern)
-print("Request with pattern:\n");
-$result = $server->call('archdat.get_names', "my pattern");
-foreach $name ( @{$result} )
+print("Request without pattern:\n");
+$results = $server->call('archiver.get_names', "");
+foreach $result ( @{$results} )
 {
-	print "Channel: '$name'\n";
+	$name = $result->{name};
+	$start = time2string($result->{start_sec}, $result->{start_nano});
+	$end   = time2string($result->{end_sec},   $result->{end_nano});
+	print("Channel $name, $start - $end\n");
 }
 
-print("\nRequest without pattern:\n");
-$result = $server->call('archdat.get_names', "");
-foreach $name ( @{$result} )
+print("Request with pattern:\n");
+$results = $server->call('archiver.get_names', "fred");
+foreach $result ( @{$results} )
 {
-	print "Channel: '$name'\n";
+	$name = $result->{name};
+	$start = time2string($result->{start_sec}, $result->{start_nano});
+	$end   = time2string($result->{end_sec},   $result->{end_nano});
+	print("Channel $name, $start - $end\n");
+}
+
+
+# result = archiver.get_values(string name[],
+#                              int32 start_sec, int32 start_nano,
+#                              int32 end_sec, int32 end_nano, int32 count,
+#                              int32 how)
+# result = (look at spec, too complex for this comment)
+print("Get Values:\n");
+@names = ( "fred", "freddy", "Jimmy", "James" );
+#@names = ( "fred" );
+$count = 10;
+$end = time();
+$start = $end - $count;
+$how = 0;
+# note: have to pass ref. to the 'names' array,
+# otherwise perl will turn it into a sequence of names:
+$results = $server->call('archiver.get_values', \@names,
+			 $start, 2, $end, 4, $count, $how);
+show_values($result);
+
+sub time2string($$)
+{
+    my ($secs, $nano) = @_;
+
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+	localtime($secs);
+    return sprintf("%02d/%02d/%04d %02d:%02d:%02d.%09ld",
+		   $mon+1, $mday, $year + 1900, $hour, $min, $sec, $nano);
+}
+
+sub show_values($)
+{
+    my ($result) = @_;
+
+    foreach $result ( @{$results} )
+    {
+	print("Result:\n");
+	%meta = %{$result->{meta}};
+	if ($meta{type} == 1)
+	{
+	    print("Display : $meta{disp_low} ... $meta{disp_high}\n");
+	    print("Alarms  : $meta{alarm_low} ... $meta{alarm_high}\n");
+	    print("Warnings: $meta{warn_low} ... $meta{warn_high}\n");
+	    print("Units   : '$meta{units}', Precision: $meta{prec}\n");
+	}
+	elsif ($meta{type} == 0)
+	{   # Didn't test this case
+	    foreach $state ( @{$meta{states}} )
+	    {
+		print("State: '$state'\n");
+	    }
+	}
+	
+	foreach $value ( @{$result->{values}} )
+	{
+	    $time = time2string($value->{secs}, $value->{nano});
+	    print("$time $value->{stat}/$value->{sevr} $value->{value}\n");
+	}
+    }
 }
 

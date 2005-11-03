b6b8e1f09d6184327002da304b8d4b614fca70f3
kasemir <kasemir>
2005-11-03 17:30:37 +0000

    Content length confusion

--- XMLRPCServer/test.sh
+++ XMLRPCServer/test.sh
@@ -24,20 +24,26 @@ echo ""
 # Run the 'info' command,
 # strip the build date etc. from the
 # output to keep what's more likely to
-# stay the same when the test runs at other sites:
+# stay the same when the test runs at other sites.
+#
+# Hmmm. 'Content-Length' changes:
+# xmlrpc-c 0.9.9 under Fedora Core 2:    "Content-length : 4794"
+# xmlrpc-c 0.9.10 under MacOS X:         "Content-length : 4797"
+# xmlrpc-c 1.03.06 under RedHat EL WS 4: "Content-length : 4782"
+
 sh cgitest.sh request.info             |
+   grep -v "^Content-length:"          |
    grep -v "^built "                   |
    grep -v "^from sources for version" |
    grep -v "^Config: "  >test/info
 compare info "archiver.info command"
 
-
-sh cgitest.sh request.archives >test/archives
+sh cgitest.sh request.archives | grep -v "^Content-length:" >test/archives
 compare archives "archiver.archives command"
 
-sh cgitest.sh request.names >test/names
+sh cgitest.sh request.names | grep -v "^Content-length:" >test/names
 compare names "archiver.names command"
 
-sh cgitest.sh request.data >test/data
+sh cgitest.sh request.data | grep -v "^Content-length:" >test/data
 compare data "archiver.data command"
 

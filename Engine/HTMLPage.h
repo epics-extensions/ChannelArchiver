4db2ab1ec94ac98932da85160ff0d0b95761b92f
kasemir <kasemir>
2006-04-06 14:51:05 +0000

    Compiling HTMLPage

--- Engine/HTMLPage.h
+++ Engine/HTMLPage.h
@@ -20,25 +20,51 @@
 #undef map
 #endif
 
+/**\ingroup Engine
+ * Helper for printing a web page to a socket.
+ */
 class HTMLPage
 {
 public:
+    /** Create web page for socket with given title.
+     *  <p>
+     *  Maybe also with a reload-refresh every given seconds.
+     */
     HTMLPage (SOCKET socket, const char *title, int refresh=0);
 
     virtual ~HTMLPage ();
 
+    /** Add a line. */
     void line (const char *line);
+
+    /** Add a line. */
     void line (const stdString &line);
 
+    /** Add verbatim output. */
     void out (const char *line);
+
+    /** Add verbatim output. */
     void out (const char *line, size_t length);
+
+    /** Add verbatim output. */
     void out (const stdString &line);
 
-    // Last column name must be 0
+    /** Start a table with column headers.
+     *  <p>
+     *  Last column name must be 0.
+     */
     void openTable (size_t colspan, const char *column, ...);
+    
+    /** Add line to table.
+     *  <p>
+     *  Number of items, ending in 0, must match call to openTable().
+     */
     void tableLine (const char *item, ...);
+    
+    /** End a table. */
     void closeTable ();
 
+    /** Should bottom menu include a 'config'  link? */
     static bool with_config;
 protected:
     SOCKET socket;

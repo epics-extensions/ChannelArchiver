1dd78b6ec5c30323e0de04543578675df1b8c700
kasemir <kasemir>
2008-03-07 15:42:34 +0000

    update

--- ExampleSetup/update_server.sh
+++ ExampleSetup/update_server.sh
@@ -6,30 +6,31 @@
 #
 # Run every two minutes
 # min hour day month weekday
-# 0-59/2 * * * * /home/scripts/update_server.sh &
+# 0-59/2 * * * * /home/xfer/update_server.sh &
 
 cd /arch
 source setup.sh
 
 LOG=/tmp/update_server.log
-DATED=/tmp/update_server_`date +%Y-%m-%d_%H:%M:%S`.log 
+DATED=/tmp/update_server_`date +%Y-%m-%d_%H:%M:%S`.log
 
 # Prevent multiple runs of the update tool
+sleep 10
 if [ -f $LOG ]
 then
     exit 0
 fi
 
+touch $LOG
+
 # Run the update
-perl scripts/update_server.pl >$LOG 2>&1
+perl scripts/update_server.pl >>$LOG 2>&1
 
 # If update_server.pl was a NOP, leaving an empty log file, remove that.
 # Otherwise rename into a dated file
 if [ -s $LOG ]
 then
-	mv $LOG $DATED
+        mv $LOG $DATED
 else
-	rm $LOG
-
+        rm $LOG
 fi
-
